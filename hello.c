f87fb1bfe311d4fcfa4d3916e528f720f6b23f89
rahatali555 <ali.rahat555@gmail.com>
2026-02-27 19:54:00 +0500

    learn about three data types

--- hello.c
+++ hello.c
@@ -3,6 +3,8 @@
 int main(){
     char name[]= "ali";
     int age = 20;
-    printf("Hello %s, you are %d!\n", name, age);
+    float height=1.75;
+    printf("Hello %s, you are %d! and %.2f meters tal\n", name, age,height);
+    
     return 0;
 }
