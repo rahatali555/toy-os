64baec28a7cd5df878ed26b2ee8bf4192b52e7de
rahatali555 <ali.rahat555@gmail.com>
2026-02-27 19:05:05 +0500

    first use of variable in c

--- hello.c
+++ hello.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
-int main(void) {
-    printf("Hello World\n");
+int main(){
+    char name[]= "ali";
+    int age = 20;
+    printf("Hello %s, you are %d!\n", name, age);
     return 0;
 }
