1bd494d21a96273e295a568767f3aa8709659f4a
rahatali555 <ali.rahat555@gmail.com>
2026-03-06 19:11:53 +0500

    now i mieed &

--- hello.c
+++ hello.c
@@ -1,13 +1,12 @@
 # include<stdio.h>
 
-int main (){
-    char name[50];
+int main(){
+    char name;
     int age;
-    printf("whats you name: \n");
-    scanf("%s", name);
-    printf("whats your age: \n");
-    scanf("%d", &age);
-    printf("my name is %s and i am %d years old", name,age);
+    printf("whats your name \n");
+    scanf("%s" name);
+    printf(" whats your age \n");
+    scanf("%d" &age);
+    printf("hello %s you are %d years old ",name, age);
     return 0;
-
 }
