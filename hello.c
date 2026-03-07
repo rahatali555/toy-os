366024ba3e6b9cb010f1b9b3de615f0b7a22e801
rahatali555 <ali.rahat555@gmail.com>
2026-03-07 12:24:58 +0500

    added get f and its use

--- hello.c
+++ hello.c
@@ -4,12 +4,15 @@ int main(){
 char name [60];
 int age;
 double gpa;
+char grade;
 printf("what is your name: \n");
-scanf("%s",name);
+fgets(name,60,stdin);
 printf("what is your age: \n");
 scanf("%d",&age);
 printf("what is your gpa: \n");
 scanf("%lf", &gpa);
-printf("your name is %s and your age is%d and your gpa is%lf",name,age,gpa);
+printf("what is your grade: \n");
+scanf(" %c", &grade);
+printf("your name is %s and your age is %d and your gpa is %lf and your grade is %c" ,name,age,gpa,grade);
 return 0;
 }
