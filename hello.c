53e7098c59318adc513ac74ba8806e717c51e811
rahatali555 <ali.rahat555@gmail.com>
2026-03-07 12:13:34 +0500

    use double

--- hello.c
+++ hello.c
@@ -3,10 +3,13 @@
 int main(){
 char name [60];
 int age;
+double gpa;
 printf("what is your name: \n");
 scanf("%s",name);
 printf("what is your age: \n");
 scanf("%d",&age);
-printf("your name is %s and your age is%d",name,age);
+printf("what is your gpa: \n");
+scanf("%lf", &gpa);
+printf("your name is %s and your age is%d and your gpa is%lf",name,age,gpa);
 return 0;
 }
