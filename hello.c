b7d1c184724f016ae3fba3554f61c14969b92fe1
rahatali555 <ali.rahat555@gmail.com>
2026-03-07 11:59:18 +0500

    first time use while loop in c

--- hello.c
+++ hello.c
@@ -1,12 +1,9 @@
 # include<stdio.h>
 
 int main(){
-    char name;
-    int age;
-    printf("whats your name \n");
-    scanf("%s" name);
-    printf(" whats your age \n");
-    scanf("%d" &age);
-    printf("hello %s you are %d years old ",name, age);
-    return 0;
+int i =3;
+while(i>0){
+    printf("hello \n");
+    i--;
+}
 }
