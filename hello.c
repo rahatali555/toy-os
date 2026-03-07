d3e0d27b30dfc3066599ab05507b69ab3495cfb8
rahatali555 <ali.rahat555@gmail.com>
2026-03-07 12:06:47 +0500

    i forget colon

--- hello.c
+++ hello.c
@@ -3,9 +3,9 @@
 int main(){
 char name [60];
 int age;
-printf("what is your name \n");
+printf("what is your name: \n");
 scanf("%s",name);
-printf("what is your age \n");
+printf("what is your age: \n");
 scanf("%d",&age);
 printf("your name is %s and your age is%d",name,age);
 return 0;
