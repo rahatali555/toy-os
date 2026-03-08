de04f605b38a48dfd879266845582119c31bceb1
rahatali555 <ali.rahat555@gmail.com>
2026-03-08 08:50:07 +0500

    modified

--- hello.c
+++ hello.c
@@ -1,9 +1,9 @@
 # include<stdio.h>
 
 int main(){
-  int i = 0;
-  while(i < 6){
-    printf("hi there\n");
-    i++;
-  }
-};
+    int i =6;
+    while (i>0){
+    printf("%d\n",i);
+    i--;}
+return 0;}
+
