1b8f6a056317c998e61cb7130f5f0fab3a05138a
rahatali555 <ali.rahat555@gmail.com>
2026-03-08 10:08:07 +0500

    added and learninh using space and big name

--- hello.c
+++ hello.c
@@ -2,12 +2,17 @@
 
 int main(){
 
-  double num1;
-   double num2;
-    printf("enter your number1: \n");
-    scanf("%lf",&num1);
-     printf("enter your number2: \n");
-     scanf("%lf",&num2);
-    printf("the answer is:%f\n",num1+num2);
-    return 0;
+ char color[30];
+ char pluralNoun[30];
+ char celebrityF[30];
+  char celebrityL[30];
+ printf("Roses are %s\n",color);
+ scanf("%s",color);
+  printf(" %s \n are blue",pluralNoun);
+ scanf("%s",pluralNoun);
+  printf("i love %s %s\n",celebrityF,celebrityL);
+ scanf("%s",celebrityF);
+ scanf("%s",celebrityL);
+ 
+ return 0;
 }
