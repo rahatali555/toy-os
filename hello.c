344c121d49f5fe28b7b798b314a3aae213cf6aa0
rahatali555 <ali.rahat555@gmail.com>
2026-03-08 08:42:20 +0500

    using while in c i made a lot of mistakes

--- hello.c
+++ hello.c
@@ -1,18 +1,9 @@
 # include<stdio.h>
 
 int main(){
-char name [60];
-int age;
-double gpa;
-char grade;
-printf("what is your name: \n");
-fgets(name,60,stdin);
-printf("what is your age: \n");
-scanf("%d",&age);
-printf("what is your gpa: \n");
-scanf("%lf", &gpa);
-printf("what is your grade: \n");
-scanf(" %c", &grade);
-printf("your name is %s and your age is %d and your gpa is %lf and your grade is %c" ,name,age,gpa,grade);
-return 0;
-}
+  int i = 0;
+  while(i < 6){
+    printf("hi there\n");
+    i++;
+  }
+};
