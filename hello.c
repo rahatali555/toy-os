15e6ee56e59e31afb84069717891e326f1083027
rahatali555 <ali.rahat555@gmail.com>
2026-03-08 09:38:35 +0500

    build small calculator

--- hello.c
+++ hello.c
@@ -2,11 +2,12 @@
 
 int main(){
 
-   char grade;
-
-      printf("what is your grade:\n");
-      scanf("%c",&grade);
-
-      printf("your grade is %c \n",grade);
-return 0;
+  double num1;
+   double num2;
+    printf("enter your number1: \n");
+    scanf("%lf",&num1);
+     printf("enter your number2: \n");
+     scanf("%lf",&num2);
+    printf("the answer is:%f\n",num1+num2);
+    return 0;
 }
