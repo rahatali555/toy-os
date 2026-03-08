e98a975e870ed338f2af696ddd2036d7cfcf68d8
rahatali555 <ali.rahat555@gmail.com>
2026-03-08 09:22:25 +0500

    indented

--- hello.c
+++ hello.c
@@ -1,10 +1,12 @@
 # include<stdio.h>
 
 int main(){
+
    char grade;
 
-   printf("what is your grade:\n");
-    scanf("%c",&grade);
-printf("your grade is %c \n",grade);
+      printf("what is your grade:\n");
+      scanf("%c",&grade);
+
+      printf("your grade is %c \n",grade);
 return 0;
 }
