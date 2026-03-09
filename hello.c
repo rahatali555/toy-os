ac2bb3efa0bc0f83ac83d91dfab658fc82b66842
rahatali555 <ali.rahat555@gmail.com>
2026-03-09 14:57:49 +0500

    again practice

--- hello.c
+++ hello.c
@@ -1,16 +1,11 @@
 # include<stdio.h>
 
 int main(){
+int i;
+for(i=6;i>=2;i--){
+    printf("hi there \n");
+    
+}
 
- char color[30];
- char pluralNoun[30];
- char celebrityF[30];
-  char celebrityL[30];
-printf("enter a color: \n"); 
-scanf("%s" , color);
-printf("enter a plural noun:\n");
-scanf("%s", pluralNoun);
-printf("enter a celebrity's name:\n");
-scanf("%s %s" , celebrityF, celebrityL);
- return 0;
+return 0;
 }
