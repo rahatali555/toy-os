ba397601c8b09f56c50af3d7ab1fa3e67314aaf0
rahatali555 <ali.rahat555@gmail.com>
2026-03-10 09:44:26 +0500

    practice forin c

--- hello.c
+++ hello.c
@@ -1,11 +1,14 @@
 # include<stdio.h>
 
+
 int main(){
-    int i =0;
-    while(i<=10){
-        printf("hi \n");
-        i++;
-    }
-    return 0;}
+   
+   int question ;
+   printf("how many times \n");
+   for(int q = 0; q < question; q++){
+    printf("hi\n");
+   } 
+   return 0;    }
+  
 
 
