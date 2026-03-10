2f178bcdeef9b8a109d1e85c0f369c884ef71c2e
rahatali555 <ali.rahat555@gmail.com>
2026-03-10 12:36:30 +0500

    array  using fix num

--- hello.c
+++ hello.c
@@ -3,9 +3,9 @@
 
 int main(){
    
-int luckyNumber[]={3,4,6,8} ;
-luckyNumber[0]=1000;
-printf("%d",luckyNumber[0]);
+int luckyNumber[5]={1,2,3,4,5};
+luckyNumber[0]=12;
+printf("%d", luckyNumber[0]);
    return 0;    }
   
 
