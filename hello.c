bf70728f6efbcb91eca04602991a7745cb09ff09
rahatali555 <ali.rahat555@gmail.com>
2026-03-14 22:00:38 +0500

    am messing up in c about using function

--- hello.c
+++ hello.c
@@ -1,7 +1,12 @@
 # include<stdio.h>
 
 double cube(double num){
-  
+  const  double result = num * num * num;
+  return result;
+}
+double main (){
+    printf("anser=%f" , cube(2.2));
+    return 0;
 }
    
 
