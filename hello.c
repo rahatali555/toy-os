e51b41bc7fc34ee2531341ea32fdcee1d8d1e063
rahatali555 <ali.rahat555@gmail.com>
2026-03-14 15:58:03 +0500

    practice

--- hello.c
+++ hello.c
@@ -1,14 +1,8 @@
 # include<stdio.h>
 
 double cube(double num){
-  double  result= num *num *num;
-  return result;
+  
 }
-int main (){
-   printf("result = %f ",cube(3.0));
-   return 0;
-}
-
    
 
 
