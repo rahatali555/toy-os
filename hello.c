e7756c506eeee7fe3a346264d4a1b2290d40b50b
rahatali555 <ali.rahat555@gmail.com>
2026-03-15 00:12:19 +0500

    practice

--- hello.c
+++ hello.c
@@ -1,13 +1,15 @@
 # include<stdio.h>
 
-double square(double num)
-   {
-    const double result= num * num;
-    return result;
-   }
+double square(double num);
+  
 int main ()
 {
     printf("answer=%f", square(2.2));
     return 0;
 }
 
+double square(double num)
+   {
+    const double result= num * num;
+    return result;
+   }
