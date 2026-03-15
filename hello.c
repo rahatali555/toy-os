2587c1297f065b692e018179ab10b5dca15dc438
rahatali555 <ali.rahat555@gmail.com>
2026-03-15 18:37:53 +0500

    practice if

--- hello.c
+++ hello.c
@@ -1,9 +1,18 @@
 # include<stdio.h>
+int max(int num1, int num2){
+    int result;
+    if(num1 > num2){
+        result=num1;
+    } else{
+        result=num2;
+    }
 
+}
 int main(){
-for(int i=0; i<4;i++){
-    printf("*\n");
+    printf("%d",max(2,3));
+    return 0;
 }
 
-}
+
+
 
