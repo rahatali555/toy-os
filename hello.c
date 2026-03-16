12c0ee9dde9981270c12a52af638574912965d9e
rahatali555 <ali.rahat555@gmail.com>
2026-03-16 09:56:21 +0500

    practicing function

--- hello.c
+++ hello.c
@@ -2,17 +2,16 @@
 int max(int num1, int num2){
     int result;
     if(num1>num2){
-        result= num1;
-
+        result = num1;
     }
     else{
-        result=2;
+        result=  num2;
+
     }
-   
-} 
-int main(){
-     printf("%d",(2,3));
-     return 0;
+}
+int main (){
+    printf("%d",(3,4));
+    return 0;
 }
 
 
